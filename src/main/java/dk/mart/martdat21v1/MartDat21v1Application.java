b33c8cdf3907732b143943e91db37dcf3b55edd6
martinlaursen97 <martin_laursen21@hotmail.com>
2021-10-08 13:42:26 +0200

    first

---
+++ src/main/java/dk/mart/martdat21v1/MartDat21v1Application.java
@@ -0,0 +1,13 @@
+package dk.mart.martdat21v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartDat21v1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MartDat21v1Application.class, args);
+    }
+
+}
---
+++ src/test/java/dk/mart/martdat21v1/MartDat21v1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.mart.martdat21v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartDat21v1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
